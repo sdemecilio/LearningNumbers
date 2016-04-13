3a49edc473a9dfbadcaae7942474eb16ccd90f22
sdemecilio <stacey.demecilio@live.com>
2016-04-13 02:15:34 -0700

    numbers are generated in play, left button check works, right button check currently crashes app

--- app/src/main/java/stacey/greenrivertech/net/learningnumbers/LearningGameModel.java
+++ app/src/main/java/stacey/greenrivertech/net/learningnumbers/LearningGameModel.java
@@ -1,5 +1,4 @@
 package stacey.greenrivertech.net.learningnumbers;
-
 import java.util.Random;
 
 /**
@@ -11,36 +10,53 @@ public class LearningGameModel
     private int secondNumber;
     private int score;
     private int gamesPlayed;
-    private int retrySecond;
+    public static final int LEFT_SIDE = 0; // different to show left
+    public static final int RIGHT_SIDE = 1; // different to show right
 
-    public LearningGameModel()
-    {
-        generate();
+    Random random = new Random();
+
+    public LearningGameModel() {
         gamesPlayed = 0;
         score = 0;
+        generate();
     }
 
-    public void generate()
-    {
-        Random random = new Random();
-
+    public void generate() {
         firstNumber = random.nextInt(10) + 1;
         secondNumber = random.nextInt(10) + 1;
+    }
 
-        if (secondNumber == firstNumber)
-        {
-            retrySecond = random.nextInt(10) + 1;
-            retrySecond = secondNumber;
+    public boolean play (int choice) {
+        gamesPlayed++;
+
+        if (choice == 0 && firstNumber > secondNumber) {
+            score++;
+            return true;
+        }
+
+        else if (choice == 1 && secondNumber > firstNumber) {
+            score++;
+            return true;
+        }
+        else {
+            return false;
         }
     }
 
-    public int getFirstNumber()
-    {
+    public int getFirstNumber() {
         return firstNumber;
     }
 
-    public int getSecondNumber()
-    {
+    public int getSecondNumber() {
         return secondNumber;
     }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getGamesPlayed() {
+        return gamesPlayed;
+    }
+
 }
--- app/src/main/java/stacey/greenrivertech/net/learningnumbers/MainScreen.java
+++ app/src/main/java/stacey/greenrivertech/net/learningnumbers/MainScreen.java
@@ -18,11 +18,7 @@ import java.util.Random;
 
 public class MainScreen extends AppCompatActivity {
 
-    Random random = new Random();
-    private int numberCorrect = 0;
-    private int playTracker = 0;
-    private int firstNumber = random.nextInt(10) + 1; // +1 allows for number between 0 and 1
-    private int secondNumber = random.nextInt(10) + 1;
+    private LearningGameModel model = new LearningGameModel();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +35,19 @@ public class MainScreen extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        model.generate();
+        Button firstButton = (Button)findViewById(R.id.firstNumber);
+        firstButton.setText("" + model.getFirstNumber());
+
+        Button secondButton = (Button)findViewById(R.id.secondNumber);
+        secondButton.setText("" + model.getSecondNumber());
+
+        TextView score = (TextView)findViewById(R.id.numberScore);
+        score.setText("" + model.getScore());
+
+        TextView gamesPlayed = (TextView)findViewById(R.id.numberPlayed);
+        gamesPlayed.setText("" + model.getGamesPlayed());
     }
 
     @Override
@@ -63,105 +72,85 @@ public class MainScreen extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /** created method for random number */
-    public void generate(View view)
+    public void checkFirstNumber (View view)
     {
-        firstNumber = random.nextInt(10) + 1;
-        secondNumber = random.nextInt(10) + 1;
+        boolean result = model.play(LearningGameModel.LEFT_SIDE);
+        model.generate();
 
-        // prints random number to button 1
-        TextView firstNum = (TextView)findViewById(R.id.firstNumber);
-        String firstNumString = String.valueOf(firstNumber);
-        firstNum.setText(firstNumString);
+        model.getFirstNumber();
+        model.getSecondNumber();
 
-        // prevent first and second number from being the same
-        if (secondNumber == firstNumber)
-        {
-            int retrySecond = random.nextInt(10) + 1;
-            secondNumber = retrySecond;
-        }
-        else
-        {
-            // print second number to button 2
-            TextView secondNum = (TextView)findViewById(R.id.secondNumber);
-            String secondNumString = String.valueOf(secondNumber);
-            secondNum.setText(secondNumString);
-        }
-
-        playTracker++; // keeps track of number of plays
-
-        // prints the number of plays to the correct section
-        TextView trackPlay = (TextView)findViewById(R.id.numberPlayed);
-        String play = String.valueOf(playTracker);
-        trackPlay.setText(play);
-
-        // after clicking the "generate" button for first round,
-        // button becomes disabled and numbers are automatically regenerated
-        Button generateNumbers = (Button)findViewById(R.id.generateNumbers);
-        generateNumbers.setEnabled(false);
-    }
-
-    /** check to see if firstNumber is bigger than secondNumber */
-    public void checkFirstNumber (View view) {
-
-        if (firstNumber > secondNumber) {
-            numberCorrect++;
-
-            // toast which sends a message that answer is correct
-            CharSequence text = "That is correct!";
-            Context context = getApplicationContext();
-            int duration = Toast.LENGTH_SHORT;
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
-        } else {
-            Context context = getApplicationContext();
-            int duration = Toast.LENGTH_SHORT;
-            CharSequence text = "That is not right. :(";
-
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
-
-        }
-
-        TextView score = (TextView) findViewById(R.id.numberScore);
-        String stringScore = String.valueOf(numberCorrect);
-        score.setText(stringScore);
-
-        generate(view); // automatically regenerates numbers
-
-    }
-
-    /** check to see if secondNumber is bigger than firstNumber */
-    public void checkSecoundNumber (View view)
-    {
-        if (secondNumber > firstNumber)
-        {
-
-            numberCorrect++;
-
-            // toast which sends a message that answer is correct
-            CharSequence text = "That is correct!";
-            Context context = getApplicationContext();
-            int duration = Toast.LENGTH_SHORT;
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
-        }
-
-        else
-        {
-            Context context = getApplicationContext();
-            int duration = Toast.LENGTH_SHORT;
-            CharSequence text = "That is not right. :(";
-
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
-
-        }
-
+        model.getScore();
         TextView score = (TextView)findViewById(R.id.numberScore);
-        String stringScore = String.valueOf(numberCorrect);
-        score.setText(stringScore);
+        score.setText("" + model.getScore());
 
-        generate(view); // automatically regenerates numbers
+        Button leftButton = (Button)findViewById(R.id.firstNumber);
+        leftButton.setText("" + model.getFirstNumber());
+
+        Button rightButton = (Button)findViewById(R.id.secondNumber);
+        rightButton.setText("" + model.getSecondNumber());
+
+        TextView gamesPlayed = (TextView)findViewById(R.id.numberPlayed);
+        score.setText("" + model.getGamesPlayed());
+
+        if (result == true)
+        {
+            Context context = getApplicationContext();
+            int duration = Toast.LENGTH_SHORT;
+            CharSequence text = "That's correct!";
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+        else
+        {
+            Context context = getApplicationContext();
+            int duration = Toast.LENGTH_SHORT;
+            CharSequence text = "That is not correct :(";
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+    }
+
+    public void checkSecondNumber(View view)
+    {
+        boolean result = model.play(LearningGameModel.RIGHT_SIDE);
+
+        model.generate();
+        model.getFirstNumber();
+        model.getSecondNumber();
+
+        model.getScore();
+        TextView score = (TextView)findViewById(R.id.numberScore);
+        score.setText("" + model.getScore());
+
+        Button leftButton = (Button)findViewById(R.id.firstNumber);
+        leftButton.setText("" + model.getFirstNumber());
+
+        Button rightButton = (Button)findViewById(R.id.secondNumber);
+        rightButton.setText("" + model.getSecondNumber());
+
+        TextView gamesPlayed = (TextView)findViewById(R.id.numberPlayed);
+        score.setText("" + model.getGamesPlayed());
+
+        if (result == true)
+        {
+            Context context = getApplicationContext();
+            int duration = Toast.LENGTH_SHORT;
+            CharSequence text = "That's correct!";
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+        else
+        {
+            Context context = getApplicationContext();
+            int duration = Toast.LENGTH_SHORT;
+            CharSequence text = "That is not correct :(";
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
     }
 }
